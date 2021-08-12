889c6f64c73472e3425627927b54db702da3cd5d
Strategically Nicole <36245900+strategicallynicole@users.noreply.github.com>
2021-08-12 02:10:16 -0400

    Initial commit

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <z-canvas :views="$options.components"></z-canvas>
+</template>
+<script>
+import home from "/components/home";
+import foo from "/components/foo";
+import og from "/components/floors/og";
+import eg from "/components/floors/eg";
+
+export default {
+  components: {
+    home,
+    eg,
+    og,
+    foo
+  },
+  mounted() {
+    this.$zircle.setView("home");
+  }
+};
+</script>
+<style>
+</style>
